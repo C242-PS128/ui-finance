ffde2c9161b203b5d91a6a4284e4d128ebfb716a
arifinsyarifudin787 <arifinnurmuhammad787@gmail.com>
2024-12-10 12:49:09 +0700

    bottom navigation dan lain lain

---
+++ app/src/main/java/com/example/myapplication/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.login
+
+import androidx.fragment.app.Fragment
+
+class LoginActivity: Fragment (R.layout.login) {
+    //
+}
---
+++ app/src/main/java/com/example/myapplication/ui/add_savings/AddSavings.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.ui.add_savings
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class AddSavings : Fragment (R.layout.add_savings) {
+    // Tambahkan logika terkait Financial Freedom di sini
+}
