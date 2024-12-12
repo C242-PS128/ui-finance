a247593a7610c20b3a4367fc56bbe618b5506187
arifinsyarifudin787 <95580420+arifinsyarifudin787@users.noreply.github.com>
2024-12-12 17:52:26 +0700

    Update LoginActivity.kt

--- app/src/main/java/com/example/myapplication/login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/login/LoginActivity.kt
@@ -137,66 +137,7 @@ class LoginActivity: AppCompatActivity() {
     companion object {
         private const val TAG = "LoginActivity"
     }
-//    private var binding:ActivityLoginBinding? = null
-//    private lateinit var auth: FirebaseAuth
-//
-//    private lateinit var googleSignInClient: GoogleSignInClient
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        var binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding?.root)
-//        auth = Firebase.auth
-//
-//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-//            .requesIdToken(getString(R.string.default_web_client_id))
-//            .requestEmail()
-//            .build()
-//    }
 
-
-//    private fun setContentView() {
-//        TODO("Not yet implemented")
-//    }
-//    () {
-
-//    auth.createUserWithEmailAndPassword(email, password)
-//    .addOnCompleteListener(this) { task ->
-//        if (task.isSuccessful) {
-//            // Sign in success, update UI with the signed-in user's information
-//            Log.d(TAG, "createUserWithEmail:success")
-//            val user = auth.currentUser
-//            updateUI(user)
-//        } else {
-//            // If sign in fails, display a message to the user.
-//            Log.w(TAG, "createUserWithEmail:failure", task.exception)
-//            Toast.makeText(
-//                baseContext,
-//                "Authentication failed.",
-//                Toast.LENGTH_SHORT,
-//            ).show()
-//            updateUI(null)
-//        }
-//    }
-//
-//    auth.signInWithEmailAndPassword(email, password)
-//    .addOnCompleteListener(this) { task ->
-//        if (task.isSuccessful) {
-//            // Sign in success, update UI with the signed-in user's information
-//            Log.d(TAG, "signInWithEmail:success")
-//            val user = auth.currentUser
-//            updateUI(user)
-//        } else {
-//            // If sign in fails, display a message to the user.
-//            Log.w(TAG, "signInWithEmail:failure", task.exception)
-//            Toast.makeText(
-//                baseContext,
-//                "Authentication failed.",
-//                Toast.LENGTH_SHORT,
-//            ).show()
-//            updateUI(null)
-//        }
-//    }
 }
 
 interface ActivityLoginBinding {
